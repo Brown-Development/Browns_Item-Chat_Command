b71580361e435a452a8bdf0e20f46b291f2df1be
Brown Development <130308145+Brown-Development@users.noreply.github.com>
2023-10-17 11:40:56 -0700

    Added QB/ESX Compatibility

--- config.lua
+++ config.lua
@@ -1,10 +1,10 @@
 Config = {}    
--- Configure As Many Items as you want
 
--- Each Item can only perform One Chat Command (You cant add multiple commands for the same item)
+Config.Framework = 'qbcore' -- 'qbcore' or 'esx'
 
-
+--**Configure As Many Items as you want**--
+---Each Item can only perform One Chat Command (You cant add multiple commands for the same item)---
 Config.Setup = {
-    [1] = {command = "revive", itemname = "lcd-lsd"},
-    [2] = {command = "kill", itemname = "lcd-heroin"},
+    [1] = {command = "me eats a steak", itemname = "steak"}, -- FOR EXAMPLE: this would do a chat command "/me eats a steak" when a "steak" item is used
+    [2] = {command = "me drinks a glass of water", itemname = "glass_of_water"}, -- FOR EXAMPLE: this would do a chat command "/me drinks a glass of water" when a "glass_of_water" item is used
 }
